6ce654cc02145003fdbef4fcdacc3799f05be615
aitumik <aitumik@protonmail.com>
2026-02-04 01:34:41 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Autograd Engine
+
+Learning Deep Learning by making a deep learning library
+
+
+## Resources
+* [https://github.com/tinygrad/tinygrad](https://github.com/tinygrad/tinygrad)
+
---
+++ core/tensor.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+
+#
+class Tensor:
+    def __init__(self,data):
+        self.data = data
+
+
+if __name__ == "__main__":
+    # do something here
+    print("testing tensors")
