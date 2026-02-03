595afc5a3cfa825ce11a2d41fa2a4ffa56d4fa7a
aitumik <aitumik@protonmail.com>
2026-02-04 01:56:43 +0300

    wip: starting out on the tensor

--- core/tensor.py
+++ core/tensor.py
@@ -1,11 +1,39 @@
 import numpy as np 
 
 
-#
-class Tensor:
-    def __init__(self,data):
-        self.data = data
+BYTES_PER_FLOAT32 = 4
+KB_TO_BYTES = 1024
+MB_TO_BYTES = 1024 * 1024 
 
+class Tensor:
+    """
+    Tensor, the foundation of machine learning computation. It carries the 
+    computation graph of the entire network
+    """
+
+    def __init__(self,data):
+        self.data  = np.array(data,dtype=np.float32)
+        self.shape = self.data.shape 
+        self.size  = len(self.data)
+        self.dtype = self.data.dtype
+
+    def __repr__(self):
+        """
+        String representation of the tensor for debugging
+        """
+        return f"Tensor(data={self.data},shape={self.shape})"
+
+    def __str__(self):
+        """
+        Human readable string representation of the tensor
+        """
+        return f"Tensor(data={self.data})"
+
+    def numpy(self):
+        """
+        Return the underlying np array.
+        """
+        return self.data
 
 if __name__ == "__main__":
     # do something here
