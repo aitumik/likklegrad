c3731697c198835583b62b62f9939e22cdd22253
aitumik <aitumik@protonmail.com>
2026-02-04 17:05:57 +0300

    wip: activations

---
+++ core/activation.py
@@ -0,0 +1,8 @@
+from .tensor import Tensor 
+
+class Softmax:
+    def forward(self, x: Tensor,dim: int = -1) -> Tensor:
+        """
+        Apply softmax activation along specified dimension
+        """
+        pass
